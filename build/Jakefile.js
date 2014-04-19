1ab88a9f7abae7ef73b7cb2dd30a31ec45c94688
Clayton Hunt <claytonhunt_104@hotmail.com>
2014-04-19 11:17:11 -0400

    All javascript outside of node_modules will now be linted

--- build/Jakefile.js
+++ build/Jakefile.js
@@ -1,20 +1,38 @@
-task("default", ["Build Started", "lint", "Build Finished"], function() {
+(function (task, require, console) {
+    'use strict';
 
-});
+    task("default", ["Build Started", "lint", "Build Finished"], function () {
 
-task("lint", [], function() {
-    var lint = require('./lint/lint_runner.js');
-    lint.validateFile('./build/Jakefile.js', {});
-});
+    });
 
-task("Build Started", function() {
-    console.log("Build Started");
-    console.log("==============");
-    console.log("");
-});
+    task("lint", [], function () {
+        var lint = require('./lint/lint_runner.js');
+        var files = getFileList();
+        lint.validateFileList(files.toArray(), getOptions(), {});
+    });
 
-task("Build Finished", function() {
-    console.log("");
-    console.log("Build Finished");
-    console.log("==============");
-});
+    task("Build Started", function () {
+        console.log("Build Started");
+        console.log("==============");
+        console.log("");
+    });
+
+    task("Build Finished", function () {
+        console.log("");
+        console.log("Build Finished");
+        console.log("==============");
+    });
+
+    function getFileList() {
+        var files = new jake.FileList();        
+        files.include('**/*.js');
+        files.exclude('node_modules');
+        return files;
+    }
+
+    function getOptions() {
+        return {
+            node: true
+        };
+    }
+}(task, require, console));
